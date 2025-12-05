da6ff3746938d2bbf02f677ee6b5c902f20a7794
Matheus <matheus.cbmdk@gmail.com>
2025-12-05 16:36:40 -0300

    feat: added api request with tanstack

---
+++ react-fundamentos-requests/src/hooks/use-user.ts
@@ -0,0 +1,49 @@
+import React from "react"
+import { api, fetcher } from "../helpers/api"
+import type { User } from "../models/user"
+import { useQueryClient } from "@tanstack/react-query"
+
+export default function useUser() {
+  const [user, setUser] = React.useState<User | null>(null)
+  const [requestStatus, setRequestStatus] = React.useState<'idle' | 'loading' | 'saving'>('idle')
+  const queryClient = useQueryClient()
+  
+  const getUser = React.useCallback(async(username: string) => {
+    try {
+      setRequestStatus('loading')
+
+      const data = await fetcher(`/users/${username}`)
+
+      setUser(data)
+    } catch (e) {
+      console.error(e);
+      alert('Erro ao buscar usuário');
+    } finally {
+      setRequestStatus('idle')
+    }
+  }, [])
+
+  async function createUser(payload: User) {
+    try {
+      setRequestStatus('saving');
+
+      await api('/users', { method: 'POST', body: JSON.stringify(payload) })
+      
+      alert('Usuário criado com sucesso!')
+
+      await queryClient.invalidateQueries({ queryKey: ['users'] })
+    } catch (e) {
+      console.error(e)
+      alert('Erro ao criar usuaŕio')
+    } finally {
+      setRequestStatus('idle')
+    }
+  }
+
+  return {
+    user,
+    userRequestStatus: requestStatus,
+    getUser,
+    createUser
+  }
+}
